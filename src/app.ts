fa31c48f141e376ee1dd39f56d0695f02d6f4667
Fabio Nascimento <fabionascimento@smartbr.com>
2023-07-28 17:48:38 -0300

    feat: add prisma e docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import fastify from "fastify";
-
 export const app = fastify();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+prisma.user.create({
+  data: {
+    name: "Fabio Nascimento",
+    email: "fabio@gmail.com",
+  },
+});
